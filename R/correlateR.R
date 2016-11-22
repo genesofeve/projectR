df1127a260e9d11d642b60b491bb6a2ecf65eff1
genesofeve <gsteinobrien@gmail.com>
2016-11-22 14:57:19 -0500

    added 2nd error message to correlateR

--- R/correlateR.R
+++ R/correlateR.R
@@ -1,7 +1,7 @@
 
 
-#cor2T<-correlateR(genes="T", dat=p, threshtype="N", threshold=.7, absR=TRUE) 
-
+cor2T<-correlateR(genes="T", dat=p, threshtype="N", threshold=10, absR=TRUE) 
+lapply(cor2T,head)
 
 correlateR<-function(genes=NA, #gene or character vector of genes for reference expression pattern
 	dat=NA,
@@ -11,16 +11,18 @@ correlateR<-function(genes=NA, #gene or character vector of genes for reference 
 	... # addtion imputs to cor, such as method 
 	){
 	if(threshtype=="N" & threshold<1){
-		stop('Threshold must be integer greater than 1 for type "N"')
+		stop('Threshold must be integer greater than 1 for threshold type "N"')
+	} else if(threshtype=="R" & threshold>1){
+		stop('Threshold must be between -1 and 1 for threshold type "R"')
 	}
 	cor2gene<-apply(dat, 1, function(G) cor(t(p[genes,]), G, ...))
 	if(absR){
 		if(threshtype=="R"){
-			corGS<-list("PositiveCOR"=sort(cor2gene,decreasing=TRUE)[which(sort(cor2gene,decreasing=TRUE)>=threshold)],
-					"NegativeCOR"=sort(cor2gene,decreasing=TRUE)[which(sort(cor2gene,decreasing=TRUE)<= -threshold)])
+			corGS<-list("PositiveCOR"=sort(cor2gene,decreasing=TRUE)[sort(cor2gene,decreasing=TRUE)>=threshold],
+					"NegativeCOR"=rev(sort(cor2gene,decreasing=TRUE)[sort(cor2gene,decreasing=TRUE) <= -threshold]))
 		} else if(threshtype=="N"){
 			corGS<-list("PositiveCOR"=sort(cor2gene,decreasing=TRUE)[1:threshold],
-					"NegativeCOR"=sort(cor2gene,decreasing=TRUE)[dim(dat)[2]-threshold:dim(dat)[2]])
+					"NegativeCOR"=sort(cor2gene,decreasing=TRUE)[dim(dat)[1]:(dim(dat)[1]-threshold)])
 		}
 	} else {
 		if(threshtype=="R"){
