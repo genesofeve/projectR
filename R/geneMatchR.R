e9cba78d4324c31df8c4fbbcf8f43f8bf5a104d9
genesofeve <gsteinobrien@gmail.com>
2017-01-30 08:59:37 -0500

    added geneMatchR fn

---
+++ R/geneMatchR.R
@@ -0,0 +1,24 @@
+
+geneMatchR<-function(
+  data1=NA,# a dataset of genes by samples 
+  AnnotionObj=NA,#an annotion object for data1. If NA, the rownames of data will be used.
+  IDcol="GeneSymbol",#the column of AnnotionData object corresponding to identifiers matching the rownames of data2
+  data2=NA # dataset with rownames to be matched
+){
+  if(!is.na(AnnotionObj)){
+    uniEGids=unique(AnnotionObj[,IDcol][AnnotionObj[,IDcol]%in%rownames(data2)])
+    rows1=match(uniEGids,AnnotionObj[,IDcol])
+    rnP<-AnnotionObj[rows1,IDcol]
+  } else {
+    uniEGids=unique(rownames(data)[rownames(data)%in%rownames(data2)])
+    rows1=match(uniEGids,rownames(data))
+    rnP<-rownames(data[rows1,])
+  }
+  rows2=match(uniEGids,rownames(data2))
+  data <- as.matrix(data)
+  p2P <- as.matrix(data[rows1,])
+  rownames(p2P) <- rnP
+  As4P <- data2[rows2,]
+  p4P <- p2P[match(rownames(p2P),rownames(As4P)),]
+  return(list("data1"=As4P,"data2"=p4P))
+}
