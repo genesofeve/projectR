07933e91803ee5c713de4ee401ed73d5c3478211
Genevieve Stein-O'Brien <genesofeve@vert.local>
2017-10-05 10:46:57 -0400

    added annotation for data objects
    Former-commit-id: fde5a4f2693c08395900b6633e250209e590a7c6

---
+++ R/map.RNAseq6l3c3t.R
@@ -0,0 +1,10 @@
+#' RNAseqing from human 3 iPS & 3 ES cell lines
+#' in 3 experimental condition at 3 time points
+#'
+#' map.RNAseq6l3c3 contains gene annotations for polyA bulk sequencing of 6 cell lines
+#' in 3 experimental condition at 3 time points. 
+#' 
+#' @name  map.RNAseq6l3c3t
+#' @docType data
+#' @format A data frames with 108 rows and 54 variables:
+"map.RNAseq6l3c3t"
