3dde8a3b171e3b5df63fa63f266b8a9e2831e3a7
Gaurav <gameider12@gmail.com>
2019-02-22 11:47:33 -0500

    alluvial modified to remove minor error
    Former-commit-id: b339f73ee8167baa89ca0bd93ea9b59e8150306e

--- R/alluvial_mat.R
+++ R/alluvial_mat.R
@@ -2,6 +2,7 @@
 #'
 #' @description Function to compute alluvial matrix
 #' @param   new.projections new projection to visualize
+#' @param
 #' @param   ct_anno Cell type annotation 
 #' @return  A matrix
 #' 
@@ -11,7 +12,7 @@
 #' @export
 
 #plot logical to ask if the plot should be generated or not
-alluvial_mat<-function(new.projections=NA, ct_anno=NA){
+alluvial_mat<-function(new.projections, ct_anno){
   sigPatternIdx<-apply(new.projections$pval,1,function(x){if(min(x,na.rm=TRUE)<=0.05){return(TRUE)} else{return(FALSE)}})
   new.projections$qval<-t(apply(new.projections$pval,1,function(x){p.adjust(x,method="BH")}))
   sigPatternIdx<-apply(new.projections$qval,1,function(x){if(min(x,na.rm=T)<=0.01){return(TRUE)} else{return(FALSE)}})
@@ -25,7 +26,7 @@ alluvial_mat<-function(new.projections=NA, ct_anno=NA){
   colnames(pattern_cells)<-c("nCells_per_pattern")
 
   DM.summary<- DM %>%
-    dplyr::select("celltype",starts_with("Pattern")) %>%
+    dplyr::select("celltype",starts_with("Patt")) %>%
     melt(id.vars=c('celltype'))
   DM.summary$value<-as.numeric(DM.summary$value)
   DM.summary<- as_tibble(DM.summary) %>%
