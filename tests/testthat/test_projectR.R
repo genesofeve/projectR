a67939bef08b03276deae8430a08f5b22f44cd27
Gaurav <gameider12@gmail.com>
2019-03-01 07:16:51 -0500

    tests modified
    Former-commit-id: 165d6b9e7f5b5615de8622f92b3d10add32049a0

--- tests/testthat/test_projectR.R
+++ tests/testthat/test_projectR.R
@@ -5,5 +5,5 @@ test_that("data is proper",{
 	expect_that(p.ESepiGen4c1l$mRNA.Seq,is_a('matrix'))
 	expect_that(p.ESepiGen4c1l, is_a('list'))
 	expect_that(AP.RNAseq6l3c3t$Amean, is_a('matrix'))
-	expect_that(dim(AP.RNAseq6l3c3t),equals(c(108,5)))
+	expect_that(all(dim(AP.RNAseq6l3c3t$Amean) == c(108,5)),is_true())
 	})
