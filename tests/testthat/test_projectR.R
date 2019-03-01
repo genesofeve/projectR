37e0a9c6e92df841e2f6f4863f047f0817585e2a
Gaurav <gameider12@gmail.com>
2019-03-01 08:03:49 -0500

    data sanctity tests added
    Former-commit-id: d4a4d9b64b288779826b5a1ffe84e750a4265a1e

--- tests/testthat/test_projectR.R
+++ tests/testthat/test_projectR.R
@@ -4,6 +4,17 @@ test_that("data is proper",{
 
 	expect_that(p.ESepiGen4c1l$mRNA.Seq,is_a('matrix'))
 	expect_that(p.ESepiGen4c1l, is_a('list'))
+	expect_that(length(p.ESepiGen4c1l),equals(6))
+	expect_that(map.ESepiGen4c1l, is_a('data.frame'))
+	expect_that(all(dim(map.ESepiGen4c1l) == c(93,9)),is_true())
+	expect_that(map.ESepiGen4c1l$GeneSymbols, is_a('character'))
+	expect_that(all(class(AP.RNAseq6l3c3t) == c('list','CoGAPS')),is_true())
+	expect_that(length(AP.RNAseq6l3c3t),equals(12))
 	expect_that(AP.RNAseq6l3c3t$Amean, is_a('matrix'))
 	expect_that(all(dim(AP.RNAseq6l3c3t$Amean) == c(108,5)),is_true())
+	expect_that(pd.ESepiGen4c1l,is_a('data.frame'))
+	expect_that(all(dim(pd.ESepiGen4c1l) == c(9,2)),is_true())
+	expect_that(pd.RNAseq6l3c3t,is_a('data.frame'))
+	expect_that(all(dim(pd.RNAseq6l3c3t) == c(54,38)),is_true())
 	})
+
