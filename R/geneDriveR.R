c71777ab3f17e448ddaa79edab8a15f640998b5e
Jared Slosberg <jslosbe1@jhmi.edu>
2021-04-20 20:03:22 -0400

    first pass at implementing projectionDrivers for two cell groups

--- R/geneDriveR.R
+++ R/geneDriveR.R
@@ -2,29 +2,65 @@
 #' @import
 #' @importFrom 
 #' @param 
+###STATS FUNCTIONS HERE
+
+
+#######################################################################################################################################
+#' @import
+#' @importFrom 
+#' @param cellgroup1 gene x cell count matrix for cell group 1
+#' @param cellgroup2 gene x cell count matrix for cell group 2
+#' @param loadings A matrix of continuous values defining the features
+#' @param feature_name column of loadings for which drivers will be calculated.
+#' @param alpha confidence value for the bonferroni confidence intervals
+#' @param loadingsNames a vector with names of loading rows. Defaults to rownames.
 #' @rdname 
-#' @aliases 
+#' 
 #' 
 .drivers_matrix<-function(
-  data, # a dataset to be projected onto
+  cellgroup1, #gene x cell count matrix for cell group 1
+  cellgroup2, #gene x cell count matrix for cell group 2
   loadings, # a matrix of continous values to be projected with unique rownames
-  dataNames = NULL, # a vector with names of data rows
-  loadingsNames = NULL, # a vector with names of loadings rows
-  NP=NA, # vector of integers indicating which columns of loadings object to use. The default of NP=NA will use entire matrix.
+  loadingsNames = NULL # a vector with names of loadings rows
+  
 ){
   
-  ifelse(!is.na(NP),loadings<-loadings[,NP],loadings<-loadings)
-  #if(!is.na(NP)){loadings<-loadings[,NP]} was giving warning with subset of patterns
-  #match genes in data sets
-  if(is.null(dataNames)){
-    dataNames <- rownames(data)
+  #check that alpha significance level is appropriate
+  if(alpha < 0 | alpha > 1){
+    stop("alpha must be numeric between 0 and 1")
   }
+  
+  #select specified feature to calculate drivers for
+  if(feature_name %in% colnames(loadings)){
+    feature <- loadings[,feature_name, drop = F] #data.frame
+  } else  {
+    stop(paste0(feature_name, " is not a column in provided loadings"))
+  }
+  
+  # #match genes in data sets
+  # if(is.null(dataNames)){
+  #   dataNames <- rownames(data)
+  # }
   if(is.null(loadingsNames)){
     loadingsNames <- rownames(loadings)
   }
-  dataM<-geneMatchR(data1=data, data2=loadings, data1Names=dataNames, data2Names=loadingsNames, merge=FALSE)
-  print(paste(as.character(dim(dataM[[2]])[1]),'row names matched between data and loadings'))
-  print(paste('Updated dimension of data:',as.character(paste(dim(dataM[[2]]), collapse = ' '))))
   
-setMethod("geneDriveR",signature(data="matrix",loadings="matrix"),.drivers_matrix)
+  #shared rows in two data matrices
+  filtered_data <-geneMatchR(data1=cellgroup1, data2=cellgroup2, data1Names=NULL, data2Names=NULL, merge=FALSE)
+  print(paste(as.character(dim(filtered_data[[2]])[1]),'row names matched between datasets'))
+  
+  cellgroup1 <- filtered_data[[2]] #TODO: geneMatchR flips the indexes, correct?
+  cellgroup2 <- filtered_data[[1]]
+  
+  #shared rows in data matrices and loadings
+  filtered_weights <- geneMatchR(data1 = cellgroup1, data2 = feature, data1Names = NULL, data2Names = NULL, merge = F)
+  print(paste(as.character(dim(filtered_weights[[2]])[1]),'row names matched between data and loadings'))
+  print(paste('Updated dimension of data:',as.character(paste(dim(filtered_weights[[2]])[1], collapse = ' '))))
+  
+  feature_filtered <- filtered_weights[[1]]
+  cellgroup1_filtered <- filtered_weights[[2]]
+  
+}  
+  
+#setMethod("geneDriveR",signature(data="matrix",loadings="matrix"),.drivers_matrix)
 
