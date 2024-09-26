5b1a15a4615792e718d3a06f67b84e5b9a497d6e
dimalvovs <dmitrijs.lvovs@gmail.com>
2024-09-26 17:09:08 -0400

    fix no definition for class “LinearEmbeddingMatrix” warning

--- R/projectR.R
+++ R/projectR.R
@@ -136,6 +136,7 @@ setMethod("projectR",signature(data="dgCMatrix",loadings="matrix"),function(
 
 #######################################################################################################################################
 #' @import limma
+#' @import SingleCellExperiment
 #' @importFrom NMF fcnnls
 #' @examples
 #' library("CoGAPS")
